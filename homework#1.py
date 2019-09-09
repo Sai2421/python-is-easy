5c5fa90cb4660649cf2c75138d636d4f733412d0
Saisaran mummidi <37036297+Sai2421@users.noreply.github.com>
2019-09-09 13:45:34 +0530

    Add files via upload

---
+++ homework#1.py
@@ -0,0 +1,21 @@
+#This python file has details about a song
+
+"""
+"Happier" is a song by American music producer Marshmello and British band Bastille. 
+Written and produced by Marshmello, with lyrics from Dan Smith and Steve Mac
+"""
+Song = "Happier"
+Band = "Bastille"
+YearOfRelease = 2018
+DurationInSeconds = 214
+songwriter = "Christopher Comstock, Dan Smith, Steve Mac"
+Producer= "Marshmello"
+Genre="Pop"
+
+print(Song)
+print(Band)
+print(YearOfRelease)
+print(DurationInSeconds)
+print(Producer)
+print(songwriter)
+print(Genre)
